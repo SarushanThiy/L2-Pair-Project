4d632943be6cbe5721484465754e9deaf0ee959f
Sarushan Thiyakumar <sarushan.thiy@gmail.com>
2022-04-06 12:36:56 +0100

    added more functions into models/post

--- api/models/Post.js
+++ api/models/Post.js
@@ -11,12 +11,37 @@ class Post {
     static get all(){
         return new Promise(async (resolve, reject) => {
             try {
-                let result = await db.query(`SELECT * FROM posts`);
-                let posts = result.rows.map(r => new Post(r))
-                resolve(posts)
+                const result = await db.query(`SELECT * FROM posts`);
+                const posts = result.rows.map(r => new Post(r))
+                resolve(posts);
             } catch (err){
                 reject(`Error retrieving posts: ${err}`)
-            }
-        })
-    }
+            };
+        });
+    };
+
+    static findById(id){
+        return new Promise (async (resolve, reject) => {
+            try {
+                let postData = await db.query(`SELECT * FROM posts WHERE id = $1;`, [ id ]);
+                let post = new Post(postData.rows[0]);
+                resolve(post);
+            } catch (err) {
+                reject('Post not found');
+            };
+        });
+    };
+
+    static create(title, name, body){
+        return new Promise (async (resolve, reject) => {
+            try {
+                let postData = await db.query(`INSERT INTO posts (title, name, body) VALUES ($1, $2, $3), RETURNING *;`, [title, name, body]);
+                resolve(post);
+            } catch (err){
+                reject ('Post could not be created');
+            };
+        });
+    };
 }
+
+module.exports = Post;
