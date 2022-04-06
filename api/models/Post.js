30bd49beb5140618df2fe4e37559246b2042042f
Sarushan Thiyakumar <sarushan.thiy@gmail.com>
2022-04-06 12:23:38 +0100

    added model object and get all function

--- api/models/Post.js
+++ api/models/Post.js
@@ -2,6 +2,21 @@ const db = require('../dbConfig/init');
 
 class Post {
     constructor(data){
-        
+        this.id = data.id
+        this.title = data.title
+        this.name = data.name
+        this.body = data.body
+    }
+
+    static get all(){
+        return new Promise(async (resolve, reject) => {
+            try {
+                let result = await db.query(`SELECT * FROM posts`);
+                let posts = result.rows.map(r => new Post(r))
+                resolve(posts)
+            } catch (err){
+                reject(`Error retrieving posts: ${err}`)
+            }
+        })
     }
 }
