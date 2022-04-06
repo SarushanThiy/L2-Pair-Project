82e60f0a4ac2bad8a5fa48c20136892412ce5c9b
Sarushan Thiyakumar <sarushan.thiy@gmail.com>
2022-04-06 20:45:59 +0100

    changed variable name to pname and db name in compose file

--- db/setup/posts.sql
+++ db/setup/posts.sql
@@ -1,10 +1,10 @@
-DROP TABLE IF EXISTS posts;
+-- DROP TABLE IF EXISTS posts;
 
 CREATE TABLE posts (
     id serial PRIMARY KEY,
-    title varchar(20) NOT NULL,
-    pname varchar(120) NOT NULL,
-    body varchar(140) NOT NULL
+    title VARCHAR(20) NOT NULL,
+    pname VARCHAR(120) NOT NULL,
+    body VARCHAR(140) NOT NULL
 );
 
 
